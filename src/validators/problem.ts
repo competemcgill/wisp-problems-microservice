456fe28dfa1145edd45ff89168be2e647217a65c
Valerian Clerc <valerian.clerc@mail.mcgill.ca>
2020-05-10 15:42:00 -0400

    Data model improvements, route/validation, and manual testing (#3)
    * Added Problem + ProblemSet models
    
    * Specify indexing in schemas
    
    * Add problem hash util and DB interaction
    
    * Add ProblemSet
    
    * Add route validators
    
    * Add difficulty and title to problems
    
    * Data model modifications based on new requirements

--- src/validators/problem.ts
+++ src/validators/problem.ts
@@ -13,18 +13,20 @@ export function problemValidator(method: string): ValidationChain[] {
         }
         case "POST /problems": {
             return [
+                body("title", "Invalid or missing 'title'").isString().exists(),
                 body("source", "Invalid or missing 'source'").isString().exists(),
                 body("sourceLink", "Invalid or missing 'sourceLink'").isString().exists(),
-                body("problemSetId", "Invalid or missing 'problemSetId'").isString().exists(),
+                body("problemSetIds", "Invalid or missing 'problemSetId'").isArray().exists(),
                 body("problemMetadata", "Invalid or missing 'problemMetadata' (optional)").optional()
             ];
         }
         case "PUT /problems/:problemId": {
             return [
                 param("problemId", "Invalid or missing ':problemId'").exists().isMongoId(),
+                body("title", "Invalid or missing 'title'").isString().exists(),
                 body("source", "Invalid or missing 'source'").isString().exists(),
                 body("sourceLink", "Invalid or missing 'sourceLink'").isString().exists(),
-                body("problemSetId", "Invalid or missing 'problemSetId'").isString().exists(),
+                body("problemSetIds", "Invalid or missing 'problemSetId'").isArray().exists(),
                 body("problemMetadata", "Invalid or missing 'problemMetadata' (optional)").optional()
             ];
         }
