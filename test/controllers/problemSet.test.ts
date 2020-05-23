02e28791ede5a1bb5edb8c2433c7965318f94bec
MohamedBeydoun <mohamed.beydoun@mail.mcgill.ca>
2020-05-23 14:56:06 -0400

    Reorganized tests

--- test/controllers/problemSet.test.ts
+++ test/controllers/problemSet.test.ts
@@ -1,82 +1,31 @@
-import mongoose from "mongoose";
-import chaiHttp from "chai-http";
-import chaiAsPromised from "chai-as-promised";
+// TODO: mock DB interactions
 import chai, { expect } from "chai";
-import { app, port } from "../../src/app";
-import { Problem, IProblemModel } from "../../src/database/models/problem";
 import { IProblem, Difficulty, PlatformName } from "../../src/interfaces/IProblem";
 import { IProblemSet } from "../../src/interfaces/IProblemSet";
-import { problemDBInteractions } from "../../src/database/interactions/problem";
-import { problemSetDBInteractions } from "../../src/database/interactions/problemSet";
-import { ProblemSet, IProblemSetModel } from "../../src/database/models/problemSet";
+import { problemSetController } from "../../src/controllers/problemSet";
 
-chai.use(chaiHttp);
-chai.use(chaiAsPromised);
-
-let server: import("http").Server;
-let testProblem: IProblemModel;
-let testProblemSet: IProblemSetModel;
-let dbUrl = "";
-(process.env.DB_URL)
-    ? dbUrl = process.env.DB_URL
-    : dbUrl = "mongodb://mongo:27017/wisp_test";
-
+const testProblemSet: IProblemSet = {
+    title: "Test Problem Set",
+    description: "Test problem set description.",
+    tags: ["Dynamic programming", "Test"],
+    problemCount: 1
+};
+const testProblem: IProblem = {
+    title: "Test Problem",
+    problemId: "27796f7b6717753aeee14bb4ba2acf6f55c88956",
+    source: PlatformName[0],
+    sourceLink: "https://codeforces.com/problemset/problem/1/A",
+    problemSetIds: [],
+    problemMetadata: {
+        platformProblemId: "1A",
+        difficulty: Difficulty[0]
+    }
+};
 
 describe("Problems controller tests", () => {
-    before(async () => {
-        await mongoose.connect(dbUrl, {
-            useNewUrlParser: true,
-            useFindAndModify: false,
-            useUnifiedTopology: true,
-            useCreateIndex: true
-        });
-
-        await ProblemSet.deleteMany({});
-        await Problem.deleteMany({});
-
-        server = app.listen(port);
-    });
-
-    beforeEach(async () => {
-        const testProblemSetData: IProblemSet = {
-            title: "Test Problem Set",
-            description: "Test problem set description.",
-            tags: ["Dynamic programming", "Test"],
-            problemCount: 1
-        };
-
-        testProblemSet = await problemSetDBInteractions.create(testProblemSetData);
-
-        const testProblemData: IProblem = {
-            title: "Test Problem",
-            problemId: "27796f7b6717753aeee14bb4ba2acf6f55c88956",
-            source: PlatformName[0],
-            sourceLink: "https://codeforces.com/problemset/problem/1/A",
-            problemSetIds: [testProblemSet._id],
-            problemMetadata: {
-                platformProblemId: "1A",
-                difficulty: Difficulty[0]
-            }
-        };
-
-        testProblem = await problemDBInteractions.create(testProblemData);
-    });
-
-    afterEach(async () => {
-        await ProblemSet.deleteMany({});
-        await Problem.deleteMany({});
-    });
-
-    after(async () => {
-        await mongoose.disconnect();
-        server.close();
-    });
-
-    describe("GET /users", () => {
-        it("status 200: returns successfully a list of 1 problemSet", async () => {
-            const { body: problemSets } = await chai.request(app).get("/problemSets");
-            expect(problemSets.length).to.equal(1);
-            expect(problemSets[0].title).to.equal(testProblemSet.title);
+    describe("PROBLEMS: list", () => {
+        it("status 200: returns a list of problems", async () => {
+            expect(true).to.be.true;
         });
     });
 });
---
+++ test/routes/problem.test.ts
@@ -0,0 +1,26 @@
+// TODO: mock controller interactions
+import chaiHttp from "chai-http";
+import chaiAsPromised from "chai-as-promised";
+import chai, { expect } from "chai";
+import { app, port } from "../../src/app";
+
+chai.use(chaiHttp);
+chai.use(chaiAsPromised);
+
+let server: import("http").Server;
+
+describe("Problems routes tests", () => {
+    before(async () => {
+        server = app.listen(port);
+    });
+
+    after(async () => {
+        server.close();
+    });
+
+    describe("GET /problems", () => {
+        it("status 200: returns a list of problems", async () => {
+            expect(true).to.be.true;
+        });
+    });
+});
