317143c50ac924e36ae8407cd4e1982512ed8875
Imad Dodin <imad.dodin@mail.mcgill.ca>
2020-05-29 20:42:10 -0400

    Update Validation Code Style

--- src/validators/problemCustom.ts
+++ src/validators/problemCustom.ts
@@ -1,18 +1,18 @@
 import { problemSetDBInteractions } from "../database/interactions/problemSet";
 import { Difficulty, PlatformName } from "../interfaces/IProblem";
 
-export async function validateProblemSetIds(problemSetIds: Array<string>) {
+export const validateProblemSetIds = async (problemSetIds: Array<string>) => {
     const promiseArray = problemSetIds.map(problemSetId => problemSetDBInteractions.find(problemSetId));
     const problemSetArray = await Promise.all(promiseArray);
     return !problemSetArray.some(problemSet => problemSet === null);
 }
 
-export function validateProblemMetadata(problemMetadata) {
+export const validateProblemMetadata = problemMetadata => {
     return ("platformProblemId" in problemMetadata)
         && ("difficulty" in problemMetadata)
         && (problemMetadata.difficulty.toLowerCase() in Difficulty);
 }
 
-export function validateProblemPlatform(platform) {
+export const validateProblemPlatform = platform => {
     return platform.toUpperCase() in PlatformName;
 }
