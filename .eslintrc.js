012bf26549b3bb25d9a501d5b35564fea2cd9758
Ahmed Elehwany <ahmed.elehwany@mail.mcgill.ca>
2020-06-04 16:11:14 -0400

    Test pre-commit hook

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,8 @@
 module.exports = {
-	parser: "@typescript-eslint/parser", // Specifies the ESLint parser
+	parser: "@typescript-eslint/parser",
 	parserOptions: {
-		ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
-		sourceType: "module" // Allows for the use of imports
+		ecmaVersion: 2020,
+		sourceType: "module"
 	},
 	root: true,
 	plugins: [
