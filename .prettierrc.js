e15a015f4baa4babe723aaddd1d0933cc96d6355
Cole Killian <g.cole.killian@gmail.com>
2021-06-15 18:04:08 -0700

    feat: update prettierrc with tabWidth 2 for yaml

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,14 @@
 module.exports = {
-	semi: true,
-	trailingComma: "none",
-	singleQuote: false,
-	tabWidth: 4
+    semi: true,
+    trailingComma: "none",
+    singleQuote: false,
+    tabWidth: 4,
+    overrides: [
+        {
+            files: ["*.yaml"],
+            options: {
+                tabWidth: 2
+            }
+        }
+    ]
 };
